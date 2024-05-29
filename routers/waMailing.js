4e0ed87c97d9b469a102aabad4d2762392a72d47
Bektur Razzakov <brzzkv@gmail.com>
2024-05-29 11:32:36 +0600

    mailing on websocket

--- routers/waMailing.js
+++ routers/waMailing.js
@@ -1,42 +1,120 @@
 import express from "express";
-import bodyParser from "body-parser"
-import wppconnect from '@wppconnect-team/wppconnect';
+import bodyParser from "body-parser";
 import auth from "../middleware/auth.js";
-import xlsx from 'xlsx';
-import multer from "multer";
-import Mail from "../models/Mail.js";
 import schedule from "node-schedule";
+import Mail from "../models/Mail.js";
+import qrcode from 'qrcode';
+import pkg from 'whatsapp-web.js';
+import { sendClientReadyStatus } from '../index.js';
+import { sendQr } from '../index.js';
+import { clientConnected } from '../index.js';
+
+const {Client, LocalAuth} = pkg;
 
 const waMailing = express();
 waMailing.use(bodyParser.urlencoded({extended: false}));
 waMailing.use(bodyParser.json());
 
-const storage = multer.memoryStorage();
-const upload = multer({storage: storage});
-
-waMailing.get('/get_qr', async (req, res) => {
-  try {
-    // Create WPPConnect session
-    wppconnect.create({
-      session: 'sessionName', catchQR: (qrCode, asciiQR) => {
-        // Send the raw QR code data to the client
-        res.send({qrCode});
-      }, statusFind: (statusSession, session) => {
-        console.log('Status:', statusSession);
-        // You can handle different session statuses here if needed
-      }
-    }).then(client => {
-      // You can use the client instance for further operations
-      console.log('Client is ready!');
-    }).catch(error => {
-      console.error('Error during session creation:', error);
-      res.status(500).send('Error generating QR code');
-    });
-  } catch (error) {
-    console.error('Error:', error);
-    res.status(500).send('Error generating QR code');
-  }
+const client = new Client({
+  authStrategy: new LocalAuth(),
+  webVersionCache: {
+    type: "remote",
+    remotePath:
+      "https://raw.githubusercontent.com/wppconnect-team/wa-version/main/html/2.2412.54.html",
+  },
 });
+
+client.on('qr', (qr) => {
+  console.log('QR сгенерирован', qr);
+  qrcode.toDataURL(qr, (err, url) => {
+    sendQr(url);
+    if (err) {
+      console.error('Error generating QR code', err);
+    }
+  });
+});
+
+client.on('ready', () => {
+  console.log('Client is ready!');
+  sendClientReadyStatus(true);
+});
+
+client.on('authenticated', () => {
+  clientConnected();
+  console.log('Client authenticated!');
+});
+
+client.on('disconnected', () => {
+  client.initialize();
+  console.log('Client disconnected!');
+});
+
+client.initialize();
+
+export const sendToOne = async (phone_number, message) => {
+  let status = false;
+  await client.sendMessage('996' + `${phone_number}`.slice(-9) + '@c.us', message)
+  .then(() => {
+    const mail = new Mail({
+      text: message,
+      phone_number,
+      sent_at: new Date().toISOString(),
+      deliver_status: true,
+    });
+    mail.save();
+    status = true;
+  })
+  .catch(() => {
+    const mail = new Mail({
+      phone_number,
+      text: message,
+      sent_at: new Date().toISOString(),
+      deliver_status: false,
+    });
+    mail.save();
+    status = false;
+  });
+  return status;
+};
+
+export const sendToAll = async (abons, message, scheduleDate) => {
+  if (scheduleDate) {
+    schedule.scheduleJob(scheduleDate, () => {
+      void sendMessages(abons, message, scheduleDate);
+    });
+    console.log(`Message scheduled to be sent at ${scheduleDate}`);
+  } else {
+    void sendMessages(abons, message);
+  }
+};
+
+const sendMessages = async (abons, message) => {
+  for (const abon of abons) {
+    let customMessage = message;
+    Object.keys(abon).forEach(key => {
+      customMessage = customMessage.replace(`@${key}`, abon[key]);
+    });
+    await client.sendMessage('996' + `${abon[Object.keys(abon)[0]]}`.slice(-9) + '@c.us', customMessage)
+    .then(() => {
+      const mail = new Mail({
+        phone_number: abon[Object.keys(abon)[0]],
+        text: customMessage,
+        sent_at: new Date().toISOString(),
+        deliver_status: true,
+      });
+      mail.save();
+    })
+    .catch(() => {
+      const mail = new Mail({
+        phone_number: abon[Object.keys(abon)[0]],
+        text: customMessage,
+        sent_at: new Date().toISOString(),
+        deliver_status: false,
+      });
+      mail.save();
+    });
+  }
+};
 
 waMailing.get('/get_all', auth, async (req, res) => {
   try {
@@ -46,96 +124,5 @@ waMailing.get('/get_all', auth, async (req, res) => {
     res.status(e.status || 500).send({...e});
   }
 });
-
-waMailing.post('/send_to_all', auth, upload.single('excel_file'), async (req, res) => {
-  try {
-    const {message, scheduleDate} = req.body;
-    const shouldSchedule = scheduleDate && !isNaN(Date.parse(scheduleDate));
-    
-    if (shouldSchedule) {
-      const scheduledDate = new Date(scheduleDate);
-      
-      console.log(scheduleDate);
-      schedule.scheduleJob(scheduledDate, async () => {
-        await sendWhatsAppMessage(message, req.file.buffer);
-      });
-      
-      return res.status(200).json({success: true, message: 'Message scheduled successfully'});
-    } else {
-      // Execute the task immediately
-      await sendWhatsAppMessage(message, req.file.buffer);
-      
-      return res.status(200).json({success: true, message: 'Message sent successfully'});
-    }
-  } catch (error) {
-    console.error('Error sending WhatsApp message:', error);
-    res.status(500).json({success: false, error: 'Failed to send WhatsApp message'});
-  }
-});
-
-waMailing.post('/send_to_one', auth, async (req, res) => {
-  try {
-    const {phone_number, message} = req.body;
-    
-    // Create WPPConnect client
-    const client = await wppconnect.create({
-      session: 'mySession', catchQR: true, browserSessionToken: true,
-    });
-    
-    // Connect to WhatsApp
-    await client.start();
-    
-    // Send the WhatsApp message
-    await client.sendText(phone_number.replace(/\s/g, ''), message);
-    const mail = new Mail({
-      text: message, phone_number: phone_number.replace(/\s/g, ''), sent_at: new Date(), deliver_status: 'success',
-    });
-    await mail.save();
-    
-    // Disconnect from WhatsApp
-    await client.close();
-    
-    res.status(200).json({success: true, message: 'Messages sent successfully'});
-  } catch (error) {
-    console.error('Error sending WhatsApp message:', error);
-    res.status(500).json({success: false, error: 'Failed to send WhatsApp message'});
-  }
-});
-
-const sendWhatsAppMessage = async (message, fileBuffer) => {
-  const workbook = xlsx.read(fileBuffer, {type: 'buffer'});
-  const sheetName = workbook.SheetNames[0];
-  const sheet = workbook.Sheets[sheetName];
-  // const jsonData = xlsx.utils.sheet_to_json(sheet);
-  const jsonData = [{
-    'Абонент': 'Бектур', 'Лицевой счет': 1122334455, 'Баланс': '-1234.56', 'Мобильный телефон': '996 707 777 404'
-  }];
-  
-  // Create WPPConnect client
-  const client = await wppconnect.create({
-    session: 'mySession', catchQR: true, browserSessionToken: true,
-  });
-  
-  // Connect to WhatsApp
-  await client.start();
-  
-  // Iterate over each number and send the WhatsApp message
-  for (const data of jsonData) {
-    const customizedMessage = message.replace('$имя', data['Абонент'] || '?')
-    .replace('$лс', Math.floor(Number(data['Лицевой счет'])) || '?')
-    .replace('$баланс', data['Баланс'] || '?');
-    const waMessage = await client.sendText(data['Мобильный телефон'].toString().replace(/\s/g, ''), customizedMessage);
-    const mail = new Mail({
-      text: customizedMessage,
-      phone_number: data['Мобильный телефон'].toString().replace(/\s/g, ''),
-      sent_at: new Date(),
-      deliver_status: !!waMessage?.id,
-    });
-    await mail.save();
-  }
-  
-  // Disconnect from WhatsApp
-  await client.close();
-}
 
 export default waMailing;
