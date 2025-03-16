6d62f571a87ae45aa764df0392fd1ad412983ccf
Bektur Razzakov <brzzkv@gmail.com>
2025-03-16 23:47:01 +0600

    wa-mailing-api

--- index.js
+++ index.js
@@ -2,136 +2,21 @@ import express from "express";
 import config from "./config.js";
 import cors from "cors";
 import mongoose from "mongoose";
-import axios from "axios";
-import XLSX from "xlsx";
+import waMailing from "./routers/waMailing.js";
+import users from "./routers/users.js";
 
 const app = express();
-const port = 8002;
+const port = 8000;
 const corsOptions = {
   origin: "*",
 };
-const PATH_TO_EXCEL = './public/hydra_abons.xlsx';
-const login = 'asyl';
-const password = '*hjvfirf';
-let token = '';
-
-//const abons = [
-//  {
-//    ls_abon: '175050620',
-//    tag: 'кара-суу'
-//  },
-//  {
-//    ls_abon: '175078915',
-//    tag: 'кара-суу'
-//  },
-//  {
-//    ls_abon: '175078912',
-//    tag: 'кара-суу'
-//  },
-//];
 
 app.use(cors(corsOptions));
 app.use(express.json());
 app.use(express.static("public"));
-app.use(express.urlencoded({ extended: false }));
-//app.use("/mailing", waMailing);
-//app.use("/user", users);
-
-const authorize = async () => {
-  try {
-    const req = await axios.post(`https://hydra.snt.kg:8000/rest/v2/login`, {
-      session: {
-        login,
-        password
-      }
-    });
-    
-    return await req.data?.session?.token;
-  } catch (e) {
-    console.log(e?.response);
-  }
-};
-
-const assignTags = async () => {
-  try {
-    const workbook = XLSX.readFile(PATH_TO_EXCEL);
-    const sheetName = workbook.SheetNames[0];
-    const abons = XLSX.utils.sheet_to_json(workbook.Sheets[sheetName]);
-    const data = [];
-    console.log(abons);
-    
-    for (let i = 0; i < abons.length; i++) {
-      try {
-        const req = await axios(`https://hydra.snt.kg:8000/rest/v2/search?query=${abons[i]?.ls_abon}`, {
-          headers: {
-            Authorization: `Token token=${token}`
-          }
-        });
-        
-        const foundAbon = req.data?.search_results?.find(abon => abon?.vc_section === 'SEARCH_SECTION_Accounts');
-        
-        if (!!foundAbon) {
-          const n_result_id = req.data?.search_results[0]?.n_result_id;
-          
-          if (!!n_result_id) {
-            try {
-              const reqToTags = await axios(`https://hydra.snt.kg:8000/rest/v2/subjects/customers/${n_result_id}`, {
-                headers: {
-                  Authorization: `Token token=${token}`
-                }
-              });
-              const tags = [
-                ...reqToTags.data?.customer?.t_tags,
-                abons[i]?.teg
-              ];
-              
-              const updateAbonTags = await axios.put(`https://hydra.snt.kg:8000/rest/v2/subjects/customers/${n_result_id}`, {
-                customer: {
-                  t_tags: tags,
-                }
-              }, {
-                headers: {
-                  Authorization: `Token token=${token}`
-                }
-              });
-              
-              console.log(`${i}   | |  `, {
-                ls_abon: abons[i]?.ls_abon,
-                tags: updateAbonTags.data?.customer?.t_tags,
-              })
-              
-              data.push({
-                ls_abon: abons[i]?.ls_abon,
-                tags: updateAbonTags.data?.customer?.t_tags,
-              });
-            } catch (e) {
-              console.log(e);
-            }
-          }
-        }
-      } catch (e) {
-        console.log(e);
-      }
-    }
-    return data;
-  } catch (e) {
-    console.log(e?.response);
-  }
-};
-
-app.get("/assign_tags", async (req, res) => {
-  try {
-    if (!token) {
-      token = await authorize();
-    }
-    if (!!token) {
-      const abonsData = await assignTags();
-      res.json(abonsData.slice(0, 100));
-    }
-  } catch (e) {
-    console.log(e);
-  }
-});
+app.use(express.urlencoded({extended: false}));
+app.use("/mailing", waMailing);
+app.use("/user", users);
 
 const run = async () => {
   void mongoose.connect(config.db);
--- routers/waMailing.js
+++ routers/waMailing.js
@@ -104,8 +104,7 @@ const client = new Client({
   authStrategy: new LocalAuth(),
   webVersionCache: {
     type: "remote",
-    remotePath:
-      "https://raw.githubusercontent.com/wppconnect-team/wa-version/main/html/2.2412.54.html",
+    remotePath: "https://raw.githubusercontent.com/wppconnect-team/wa-version/main/html/2.2412.54.html",
   },
 });
 
